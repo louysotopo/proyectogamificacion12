dfdeaa31d52a57c97ec130930ad2e6b888f5cc40
louysotopo <louysotopo123@gmail.com>
2021-04-30 22:07:49 -0500

    avance

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
+
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Hola Mundo!'
+    titulo = "app gamificacion"
+    lista = ['footer','header','info','hola']
+    return render_template('index.html',titulo=titulo,lista=lista)
+
+@app.route('/intermedio')
+def hoja1():
+    return render_template('intermedio.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/js/vivian/codigo.js
@@ -0,0 +1,13 @@
+var getData = function(){
+	var pc01 = document.getElementById("pc01").value;
+	var pc02 = document.getElementById("pc02").value;
+	var pc03 = document.getElementById("pc03").value;
+	var pc04 = document.getElementById("pc04").value;
+	var pc05 = document.getElementById("pc05").value;
+	var pc06 = document.getElementById("pc06").value;
+	var pc07 = document.getElementById("pc07").value;
+	var pc08 = document.getElementById("pc08").value;
+
+	console.log(pc01+" "+pc02+" "+pc03+" "+pc04+" "+pc05+" "+pc06+" "+pc07+" "+pc08)
+
+}
